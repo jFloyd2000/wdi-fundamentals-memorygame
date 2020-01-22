4aee84868828d8efa0e9c5fc5d6ad300725f2aad
Jason Floyd <floydjason84@gmail.com>
2020-01-22 02:05:48 -0600

    dynamically created cards and ran into nasty bug

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,36 +1,51 @@
-let cardsInPlay = [];
-let ids = ["cardOne", "cardTwo", "cardThree", "cardFour"];
+/*variables*/
+/*used for the flipCard method*/
+let runAgain = true;
 let cards = [
   {
   	rank: "Queen",
-  	suit: "Hearts"
+  	suit: "Hearts",
+  	cardImage: "images/queen-of-hearts.png"
   },
   {
   	rank: "Queen",
-  	suit: "Diamonds"
+  	suit: "Diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
   },
   {
   	rank: "King",
-  	suit: "Diamonds"
+  	suit: "Diamonds",
+  	cardImage: "images/king-of-diamonds.png"
   },
   {
   	rank: "King",
-  	suit: "Hearts"
+  	suit: "Hearts",
+  	cardImage: "images/king-of-hearts.png"
   }
 ]
-while(cardsInPlay.length < 4){
-	cardsInPlay.push(cards[shuffle(cards.length)]);
-}
-flipCard(cardsInPlay[0].rank, cardsInPlay[0].suit);
-flipCard(cardsInPlay[1].rank, cardsInPlay[1].suit);
-endGame(cardsInPlay[0], cardsInPlay[1]);
-function shuffle(length){
-	return Math.round(Math.random() * (length - 1));
-}
-function flipCard(rank, suit)
+/*Commands*/
+createBoard();
+/*Methods*/
+Instructions
+/**
+ * changes the cards image to the front of the card
+ * @param img the img tag to change
+ */
+function flipCard(img)
 {
-	console.log("you flipped over the " + rank + " of " + suit);
+	let cardID = img.getAttribute('data-id');
+	console.log("JFDBG:flipCard ran");
+	if(runAgain)
+	{
+		runAgain = false;
+		img.setAttribute(cards[cardID].cardImage);
+	}
 }
+/**
+ * Determines the winner
+ * @param cardA the first card to be evaluated
+ * @param cardB the second card to be evaluated
+ */
 function endGame(cardA, cardB){
 	if(cardA === cardB){
 		alert("You Win");
@@ -39,46 +54,16 @@ function endGame(cardA, cardB){
 		alert("You lose");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//DO NOT USE THESE 2 METHODS THEY DO NOT WORK
-function reveal(index){
-	let src = getSrc(cardsInPlay[index]);
-	console.log(src);
-	if(src != null){
-	    document.getElementById("cardOne").src = "https://cdn.clipart.email/15e26043d4dc82675a7e9ad5d524ec52_angry-man-clipart-159545_271-351.png";//"images/" + src;
+/**
+ * Creates the img objects and appends to the gameboard div
+ * element
+ */
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+	  let img = document.createElement('img');
+	  img.setAttribute('src', 'images/back.png');
+	  img.setAttribute('data-id', i);
+	  img.addEventListener('click', flipCard(this));
+	  document.getElementById('gameboard').appendChild(img);
     }
-}
-function getSrc(name){
-	let a = "" + name;
-	let b = a.split(" ");
-	let c = "";
-	for (var i = 0; i < b.length - 1; i++) {
-		c = c + b[i] + "-";
-	console.log(c);
-	}
-	c = c + b[b.length-1];
-	console.log(c);
-	c = c.toLowerCase();
-	console.log(c);
-	c = c + ".png";
-	console.log(c);
-	return c;
 }
